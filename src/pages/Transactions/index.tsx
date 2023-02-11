7496b1478362602b3340e0ac8f4ffca6c1d8c1bd
glaubermatos <glaub.oliveira@hotmail.com>
2023-02-11 00:52:48 -0300

    Commit: Estrutura visual - Tabela de transações

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,35 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
     return (
         <div>
             <Header />
             <Summary />
+
+            <TransactionsContainer>
+                <TransactionsTable>
+                    <tbody>
+                        <tr>
+                            <td width="50%">Desenvolvimento de sites</td>
+                            <td>
+                                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+                            </td>
+                            <td>Venda</td>
+                            <td>12/02/2023</td>
+                        </tr>
+                        <tr>
+                            <td width="50%">Hamburguer</td>
+                            <td>
+                                <PriceHighlight variant="outcome">- R$ 1.2000,00</PriceHighlight>
+                            </td>
+                            <td>Venda</td>
+                            <td>13/02/2023</td>
+                        </tr>
+                    </tbody>
+                </TransactionsTable>
+            </TransactionsContainer>
         </div>
     )
 }
